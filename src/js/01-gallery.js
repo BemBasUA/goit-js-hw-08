60eed019d567512eaeeba2ba5bfbf306bd687b54
Ihor Podvalniy <bembasua@gmail.com>
2022-08-04 18:03:37 +0300

    checkpoint

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const galleryItemsMarkupEl = createGallaryItemMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML(`beforeend`, galleryItemsMarkupEl);
+
+function createGallaryItemMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>
+    `;
+    })
+    .join(``);
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
